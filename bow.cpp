2286bffcb0913dd14d16a2744cdf85f5ee0d21bd
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2019-11-21 12:07:42 +0100

    Cursor Following Movements

---
+++ bow.cpp
@@ -0,0 +1,46 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <math.h>
+#include <iostream>
+
+
+int main()
+{
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("StickFig.png");
+
+    sf::Sprite playerSprite;
+    playerSprite.setTexture(playerTexture);
+
+    playerSprite.setOrigin(50,50);
+    playerSprite.setPosition(400,300);
+    playerSprite.setRotation(0);
+
+    sf::Vector2i mouse;
+    float angle;
+    sf::Vector2f playerPosition;
+    double a, b;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        playerPosition = playerSprite.getPosition();
+        a = mouse.x - playerPosition.x;
+        b = mouse.y - playerPosition.y;
+
+        mouse = sf::Mouse::getPosition(window);
+        angle = -atan2( a , b) * 180 / 3.14;
+        playerSprite.setRotation(angle);
+        window.clear(sf::Color::White);
+        window.draw(playerSprite);
+        window.display();
+    }
+
+    return 0;
+}
