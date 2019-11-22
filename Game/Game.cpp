5f3fad80aa5e7108255f88aa1dee73ea208f5e44
minhtuan2000 <minhtuan270820000@gmail.com>
2019-11-22 09:30:58 -0500

    Game class

---
+++ Game/Game.cpp
@@ -0,0 +1,57 @@
+#include "Game.hpp"
+
+Game::Game(){
+    // Create game window
+    sf::VideoMode videoMode = sf::VideoMode(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    window = new sf::RenderWindow(videoMode, "Archers");
+
+    // Start game
+    std::cout << "Starting Game ..." << std::endl;
+    StartGame();
+}
+
+Game::~Game(){
+    delete circle;
+    delete window;
+}
+
+void Game::StartGame(){
+    // Draw a circle
+    circle = new sf::CircleShape(100.f);
+    circle -> setFillColor(sf::Color::Green);
+
+    // Start timer
+    elapsedTime = clock();
+
+    // Update Frame
+    std::cout << "Game started ..." << std::endl;
+    while (window -> isOpen())
+    {
+        sf::Event event;
+        while (window -> pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window -> close();
+        }
+
+        window -> clear();
+        UpdateFrame();
+        window -> display();
+    }
+}
+
+void Game::UpdateFrame(){
+    // std::cout << "Updating Frame at " << double(elapsedTime) / CLOCKS_PER_SEC << std::endl;
+
+    // Calculate time has passed since the last UpdateFrame
+    double time = double(clock() - elapsedTime) / CLOCKS_PER_SEC;
+    elapsedTime = clock();
+
+    // Change circle's color
+    circle -> setFillColor(sf::Color(0, 255, 0, int((double (elapsedTime) / CLOCKS_PER_SEC) * 25) % 256));
+    window -> draw(*circle);
+}
+
+void Game::EndGame(){
+
+}
---
+++ Game/Game.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <ctime>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "GameConstants.cpp"
+
+class Game {
+    private:
+        // Add constants to the class
+        GameConstants gameConstants = GameConstants();
+        // All other classes
+        // ...
+
+        // Elapsed time
+        clock_t elapsedTime;
+
+        // Game Window
+        sf::RenderWindow* window;
+        // A circle
+        sf::CircleShape* circle;
+
+    public:
+    
+        Game();
+        ~Game();
+
+        void StartGame();
+        void UpdateFrame();
+        void EndGame();
+};
