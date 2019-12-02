02924a86b99f919dfb3a5d231afbddbd7b0616d9
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2019-12-03 00:00:58 +0100

    Add files via upload

--- bow.cpp
+++ bow.cpp
@@ -1,10 +1,8 @@
-/* Author: sonal sannigrahi */
-
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <math.h>
 #include <iostream>
-
+#include <Texture.hpp>
 
 int main()
 {
@@ -14,34 +12,36 @@ int main()
     sf::Sprite playerSprite;
     playerSprite.setTexture(playerTexture);
 
-    playerSprite.setOrigin(50,50);
-    playerSprite.setPosition(400,300);
-    playerSprite.setRotation(0);
-    /*need to add while/if statement */
-    sf::Vector2i mouse;
-    float angle;
-    sf::Vector2f playerPosition;
-    double x, y;
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+        playerSprite.setOrigin(50,50);
+        playerSprite.setPosition(400,300);
+        playerSprite.setRotation(0);
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "you can see me!");
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
+        sf::Vector2i mouse;
+        float angle;
+        sf::Vector2f playerPosition;
+        double a, b;
+
+        sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+        while (window.isOpen())
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        playerPosition = playerSprite.getPosition();
-        x = mouse.x - playerPosition.x;
-        y = mouse.y - playerPosition.y;
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            playerPosition = playerSprite.getPosition();
+            a = mouse.x - playerPosition.x;
+            b = mouse.y - playerPosition.y;
 
-        mouse = sf::Mouse::getPosition(window);
-        angle = -atan2( x , y) * 180 / 3.14;
-        playerSprite.setRotation(angle);
-        window.clear(sf::Color::White);
-        window.draw(playerSprite);
-        window.display();
+            mouse = sf::Mouse::getPosition(window);
+            angle = -atan2( a , b) * 180 / 3.14;
+            playerSprite.setRotation(angle);
+            window.clear(sf::Color::White);
+            window.draw(playerSprite);
+            window.display();
+        }
     }
 
     return 0;
