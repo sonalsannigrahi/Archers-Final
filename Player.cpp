fd09d7aa7ed9f4aaa56198aa58c9c6400e35c9bc
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2019-12-05 11:37:49 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,40 +1,47 @@
-#include Player.hpp
+#include "Player.hpp"
 
 
-Player::Players(){
-	srand (time(NULL));
+Player::Player(){
 	sf::Texture* texture = new sf::Texture();
-	texture -> loadFromFile("charac_arm.png")
-	sf::Sprite* sprite = new sf::Sprite(*texture)
+	texture -> loadFromFile("Character/charac_arm.png");
+	sprite -> setTexture(*texture);
 	sf::Texture* texturem = new sf::Texture();
-	texturem -> loadFromFile("hand_p.png")
-	sf::Sprite* spritem = new sf::Sprite(*texturem)
+	texturem -> loadFromFile("Character/hand_p.png");
+	spritem -> setTexture(*texturem);
 }
 
 void Player::setWindow(sf::RenderWindow* gameWindow){
 	window = gameWindow;
 }
 
-void Player::setsize(int width, int height ){
-	playerwidth = width;
-	playerheight = height;
+void Player::setSize(int width, int height ){
+	playerWidth = width;
+	playerHeight = height;
 }
 
 void Player::updateFrame(double time){
-    sf::Vector2i mouse;
+	// need to test implementation
+    sf::Vector2i mousePosition;
     float angle;
     sf::Vector2f playerPosition;
+    sf::Vector2i windowPosition;
     double a, b;	
 
-    spritem.setOrigin(sprite.getOrigin().x,sprite.getOrigin().y);
-    playerPosition = spritem.setPosition(sprite.getPosition().x,sprite.getPosition().y);
-    spritem.setRotation(0);
+    spritem -> setOrigin(sprite -> getOrigin().x,sprite -> getOrigin().y);
+    spritem -> setPosition(sprite -> getPosition().x,sprite -> getPosition().y);
+    playerPosition = spritem -> getPosition();
+    spritem -> setRotation(0);
     sf::Event Event;
 
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-    	a = mouse.x - playerPosition.x;
-        b = mouse.y - playerPosition.y;
-        mouse = sf::Mouse::getPosition(window);
-        angle = -atan2( a , b) * 180 / 3.14;
-        spritem.setRotation(angle);
+        mousePosition = sf::Mouse::getPosition();
+        windowPosition = window -> getPosition();
+    	a = mousePosition.x - windowPosition.x - playerPosition.x;
+        b = mousePosition.y - windowPosition.y - playerPosition.y;
+        angle = (-atan2( a , b) * 180 + 180) /  3.14159265359;
+        spritem -> setRotation(angle);
+    }
+
+    window -> draw(*sprite);
+    window -> draw(*spritem);
 }
