e158529bea68aa8ccd30a34bdab7ad7cf6354bc5
Alexandre Hirsch <51078543+alexandrehirschX@users.noreply.github.com>
2019-12-06 23:39:15 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class TextinBox
+{
+    public:
+        TextinBox(int x, int y, int sizex, int sizey, string str){
+            sf::RectangleShape rectangle(sf::Vector2f(sizex,sizey));
+            sf::Text text;
+            sf::Font font;
+            if (!font.loadFromFile("AbyssinicaSIL-R.ttf"))
+            {
+                std::cout << "no font file" << std::endl;
+            }
+            text.setFont(font);
+            text.setString(str);
+            text.setCharacterSize(50);
+            text.setPosition(sf::Vector2f(x,y));
+            text.setOrigin(sf::Vector2f(sizex/2, sizey/2));
+            text.setFillColor(sf::Color::Blue);
+            rectangle.setFillColor(sf::Color::Red);
+            rectangle.setOrigin(sf::Vector2f(sizex/2, sizey/2));
+            rectangle.setPosition(sf::Vector2f(x,y));
+        }
+};
 
 
 int main()
@@ -10,22 +36,23 @@ int main()
     int recth = 60;
     int rectw = 100;
     sf::RectangleShape play(sf::Vector2f(rectw,recth));
-    play.setFillColor(sf::Color::Red);
-    play.setOrigin(sf::Vector2f(rectw/2, recth/2));
-    play.setPosition(sf::Vector2f(400,400));
-
-
+    sf::Text text;
+    TextinBox Box1(400,400,rectw,recth,"Play Game");
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed or event.type == sf::Event::KeyEvent(K)))
+
+            
+            if (event.type == sf::Event::Closed or sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
                 window.close();
+            }
         }
 
         window.clear(sf::Color::White);
-        window.draw(play);
+        window.draw(Box1.text);
+        window.draw(Box1.rectangle);
         window.display();
     }
 
