7ca29a4edb0655dbc361dbc93ccd21bd43abbdbe
Tarcisio-Teixeira <tarcisio.soares-teixeira-neto@polytechnique.edu>
2019-12-12 10:49:21 +0100

    Start_Screen implementation

---
+++ ArchersGame/Start_Screen/UI-UX.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <SFML/Graphics.hpp>
+
+#include <Menu/Menu.hpp>
+#include "GameConstants.cpp"
+#include "GameFPS.cpp"
+#include "../Background/Background.hpp"
+#include "../Water/Water.hpp"
+#include "../Birds/Birds.hpp"
+#include "../Lightning/Lightning.hpp"
+#include "../Rain/Rain.hpp"
+#include "../Balloon/Balloon.hpp"
+#include "../Fireworks/fireworks.hpp"
+
+class StartScreen {
+    private:
+        // Add constants to the class
+        GameConstants gameConstants = GameConstants();
+
+        // All other classes
+        // ...
+        GameFPS gameFPS = GameFPS();
+        Background gameBackground = Background();
+        Water gameWater = Water();
+        Lightning gameLightning = Lightning();
+        Rain gameRain = Rain();
+        std::vector<Birds*> birds;
+        std::vector<Balloon*> balloons;
+        std::vector<Fireworks*> fireworks;
+        //Button startButton = Button("start");
+        //Button settingsButton = Button("settings");
+        //Button infoButton = Button("info");
+        //Button exitGame = Button("exit");
+        Menu menu;
+        // Elapsed time
+        clock_t elapsedTime;
+
+        // Game Window
+        sf::RenderWindow* window;
+        // A circle
+        sf::CircleShape* circle;
+
+    public:
+
+        StartScreen();
+        ~StartScreen();
+        void Start();
+        void NewGame();
+        //void PlayerMode();
+        void UpdateFrame();
+        void createBird();
+        void removeBird(int id);
+        void createBalloon();
+        void removeBalloon(int id);
+        void createFireworks();
+        void removeFireworks(int id);
+};
---
+++ Start_Screen/UI-UX.cpp
@@ -0,0 +1,200 @@
+#include "StartScreen.hpp"
+
+StartScreen::StartScreen(){
+    srand(time(NULL)); // Randomize
+
+    // Create game window
+    sf::VideoMode videoMode = sf::VideoMode(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    window = new sf::RenderWindow(videoMode, "Archers");
+
+    // Send window to other classes
+    gameBackground.setWindow(window);
+    gameWater.setWindow(window);
+    gameLightning.setWindow(window);
+    gameRain.setWindow(window);
+    ////startButton.setWindow(window);
+    ////settingsButton.setWindow(window);
+    ////infoButton.setWindow(window);
+    ////exitButton.setWindow(window);
+    menu.setWindow(window);
+    menu.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    // Inititalize other variables
+    gameBackground.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    gameWater.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    gameLightning.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    gameRain.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    //startButton.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    //settingsButton.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    //infoButton.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    //exitButton.setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+
+    Start();
+}
+
+StartScreen::~Game(){
+}
+
+void StartScreen::Start(){
+    // CIRCLE TEST
+
+    // Draw a circle
+    // circle = new sf::CircleShape(100.f);
+    // circle -> setFillColor(sf::Color::Green);
+
+    // END CIRCLE TEST
+
+    // TEST BIRDS
+
+    // createBird();
+
+    // END TEST BIRDS
+
+    // Start timer
+    elapsedTime = clock();
+
+    // Update Frame
+    std::cout << "Game started ..." << std::endl;
+    while (window -> isOpen())
+    {
+        sf::Event event;
+        while (window -> pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window -> close();
+        }
+
+        window -> clear();
+        UpdateFrame();
+        window -> display();
+    }
+}
+
+void StartScreen::UpdateFrame(){
+    // std::cout << "Updating Frame at " << double(elapsedTime) / CLOCKS_PER_SEC << std::endl;
+
+    // Creating birds
+    if (rand() % gameConstants.birdRate == 0) createBird();
+
+    // Creating balloons
+    if (rand() % gameConstants.balloonRate == 0) createBalloon();
+
+    // Creating Fireworks
+    if (rand() % gameConstants.fireworkRate == 0) createFireworks();
+
+    // Calculate time has passed since the last UpdateFrame
+    double time = double(clock() - elapsedTime) / CLOCKS_PER_SEC;
+    elapsedTime = clock();
+
+    // Draw Background
+    gameBackground.updateFrame(time);
+    //std::cout << "Yo i was here" << std::endl;
+
+    // Draw Lightning
+    if (gameConstants.isLightning) gameLightning.updateFrame(time);
+
+    //Draw Fireworks
+    if (gameConstants.isFireworks){
+        int id = 0;
+        while (id < fireworks.size()){
+            if (fireworks[id] -> isAlive()){
+                fireworks[id] -> updateFrame(time);
+                id ++;
+            }
+            else removeFireworks(id);
+        }
+    }
+
+    // Draw birds
+    if (gameConstants.isBirds){
+        int id = 0;
+        while (id < birds.size()){
+            if (birds[id] -> isAlive()){
+                birds[id] -> updateFrame(time);
+                id++;
+            }
+            else removeBird(id);
+        }
+    }
+
+    // Draw balloons
+    if (gameConstants.isBalloon){
+        int id = 0;
+        //std::cout << balloons.size();
+        while (id < balloons.size()){
+            if (balloons[id] -> isActive()){
+                balloons[id] -> updateFrame(time);
+                id++;
+            }
+            else removeBalloon(id);
+        }
+    }
+
+    // Draw Rain
+    if (gameConstants.isRaining) gameRain.updateFrame(time);
+
+    // Draw Waters
+    gameWater.updateFrame(time);
+
+    // Update FPS counter
+    gameFPS.UpdateFPS(double(elapsedTime) / CLOCKS_PER_SEC);
+    std::cout << "Game is running at " << gameFPS.GetFPS() << " fps" << std::endl;
+
+    // CIRCLE TEST
+
+    // Change circle's color
+    // circle -> setFillColor(sf::Color(0, 255, 0, int((double (elapsedTime) / CLOCKS_PER_SEC) * 25) % 256));
+    // window -> draw(*circle);
+
+    // std::cout << "Yo i was here" << std::endl;
+
+    // END CIRCLE TEST
+}
+
+void StartScreen::EndGame(){
+
+}
+
+void StartScreen::createBird(){
+    Birds* bird = new Birds();
+    bird -> setWindow(window);
+    bird -> setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    birds.push_back(bird);
+}
+
+void StartScreen::removeBird(int id){
+    if (birds.size() > id){
+        delete birds[id];
+        birds[id] = birds[birds.size() - 1];
+        birds.pop_back();
+    }
+}
+
+void StartScreen::createBalloon(){
+    Balloon* balloon = new Balloon();
+    balloon -> setWindow(window);
+    balloon -> setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    balloons.push_back(balloon);
+}
+
+void StartScreen::removeBalloon(int id){
+    if (balloons.size() > id){
+        delete balloons[id];
+        balloons[id] = balloons[balloons.size() - 1];
+        balloons.pop_back();
+    }
+}
+
+void StartScreen::createFireworks(){
+    Fireworks* firework = new Fireworks();
+    firework -> setWindow(window);
+    firework -> setSize(gameConstants.WINDOW_WIDTH, gameConstants.WINDOW_HEIGHT);
+    fireworks.push_back(firework);
+}
+
+void:: StartScreen::removeFireworks(int id){
+    if (fireworks.size() > id){
+        delete fireworks[id];
+        fireworks[id] = fireworks[fireworks.size() -1];
+        fireworks.pop_back();
+    }
+}
