03b430bd4062f16fce52b9a6db182864c183f1ab
minhtuan2000 <minhtuan270820000@gmail.com>
2019-12-12 06:06:14 -0500

    fix background

--- ArchersGame/Game/GameConstants.cpp
+++ ArchersGame/Game/GameConstants.cpp
@@ -1,15 +1,15 @@
 class GameConstants{
     public:
     // Window setting
-        int WINDOW_HEIGHT = 600;
-        int WINDOW_WIDTH = 900;
+        int WINDOW_HEIGHT = 720;
+        int WINDOW_WIDTH = 1240;
     // Spawning rate
         int birdRate = 1200;
         int balloonRate = 2000;
         int fireworkRate = 500;
     // Game setting
         bool isRaining = false;
-        bool isLightning = true;
+        bool isLightning = false;
         bool isBirds = true;
         bool isBalloon = false;
         bool isFireworks = true;
