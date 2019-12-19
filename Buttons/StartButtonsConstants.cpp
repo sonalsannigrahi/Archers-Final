16badac9f5cadfe39641ee98d801ae1609a255c4
Hoang Anh Ngo <hoang-anh.ngo@polytechnique.edu>
2019-12-19 10:42:36 +0100

    update

---
+++ Buttons/StartButtonsConstants.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+class BackgroundConstants{
+public:
+    int filename_length = 4; //modify later, depending on the number of buttons
+    int chosen = 7;
+    std::string filename[4];
+
+    
+};
