6bd360d17190fa8f4c0e0312f01e22ae96100433
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2019-12-18 19:12:41 -0500

    Player files (commented)

---
+++ ArchersGame/Character/bow.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <ctime>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <math.h>
+#include <iostream>
+
+
+class Player{
+private:
+    sf::RenderWindow* window;
+    std::vector<sf::Sprite> characterSprites;
+    sf::Sprite* spriteh = new sf::Sprite();
+    sf::Sprite* spriteb = new sf::Sprite();
+    sf::Sprite* spritea = new sf::Sprite();
+    sf::Sprite* spriteo = new sf::Sprite();
+    int winWidth, winHeight;
+    double timer;
+    int counter = 0;
+
+
+public:
+    
+    Player();
+    
+    void setWindow(sf::RenderWindow* gameWindow);
+    void setSize(int width, int height);
+    void updateFrame(double time);
+};
