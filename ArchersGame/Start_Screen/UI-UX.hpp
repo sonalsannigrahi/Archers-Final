3aa1f6794933b4fe671689ea79db9afbb52c2395
Tarcisio-Teixeira <tarcisio.soares-teixeira-neto@polytechnique.edu>
2019-12-21 10:49:30 -0500

    Start Screen initial implementation

--- ArchersGame/Start_Screen/UI-UX.hpp
+++ ArchersGame/Start_Screen/UI-UX.hpp
@@ -20,44 +20,21 @@
 
 class StartScreen {
     private:
-        // Add constants to the class
-        GameConstants gameConstants = GameConstants();
-
-        // All other classes
-        // ...
         GameFPS gameFPS = GameFPS();
         Background gameBackground = Background();
-        Water gameWater = Water();
-        Lightning gameLightning = Lightning();
-        Rain gameRain = Rain();
-        std::vector<Birds*> birds;
-        std::vector<Balloon*> balloons;
-        std::vector<Fireworks*> fireworks;
-        //Button startButton = Button("start");
-        //Button settingsButton = Button("settings");
-        //Button infoButton = Button("info");
-        //Button exitGame = Button("exit");
-        Menu menu;
         // Elapsed time
         clock_t elapsedTime;
-
         // Game Window
         sf::RenderWindow* window;
         // A circle
         sf::CircleShape* circle;
 
     public:
-
         StartScreen();
         ~StartScreen();
-        void Start();
-        void NewGame();
-        //void PlayerMode();
+        void Settings();
+        void Star();
+        void Endless();
+        void Campaign();
         void UpdateFrame();
-        void createBird();
-        void removeBird(int id);
-        void createBalloon();
-        void removeBalloon(int id);
-        void createFireworks();
-        void removeFireworks(int id);
 };
