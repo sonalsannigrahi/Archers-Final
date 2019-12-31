2ba96c4fc5582af10028db7b3039a869b64d951c
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2019-12-31 05:49:43 -0500

    spear edit

---
+++ ArchersGame/Character/bow.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <ctime>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <math.h>
+#include <iostream>
+
+
+class Player{
+private:
+    sf::RenderWindow* window;
+    std::vector<sf::Sprite> characterSprites;
+    sf::Sprite* spriteh = new sf::Sprite();
+    sf::Sprite* spriteb = new sf::Sprite();
+
+    int winWidth, winHeight;
+    double timer;
+    int counter = 0;
+    float lastAngle;
+
+public:
+    
+    Player();
+    
+    void setWindow(sf::RenderWindow* gameWindow);
+    void setSize(int width, int height);
+    void updateFrame(double time);
+};
