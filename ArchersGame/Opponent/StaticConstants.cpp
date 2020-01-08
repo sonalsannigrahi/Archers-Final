03f44b22be9050a03726e802d8100a3069662cda
Sonal Sannigrahi <sannigrahisonal@gmail.com>
2020-01-08 16:55:21 -0500

    static opponent appears, hit box drawn

---
+++ ArchersGame/Opponent/StaticConstants.cpp
@@ -0,0 +1,18 @@
+
+#include <string>
+
+class StaticConstants {
+public:
+    double changetime = 0.1;
+    std::string filename[10];
+    int filenamelen = 10;
+    int statlen = 10;
+    float statSpeed = 200.f;
+    float maxHealth = 100.f;
+
+    StaticConstants(){
+        for (int i=0; i< 10; i++) {
+            filename[i] = "stat.png";
+        }
+    }
+};
---
+++ ArchersGame/Opponent/StaticOpponent.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <SFML/Graphics.hpp>
+#include "StaticConstants.cpp"
+
+class Player;
+
+class StaticOpponent{
+private:
+    StaticConstants statconstant = StaticConstants();
+    sf::RenderWindow* window;
+    std::vector<sf::Sprite> statSprites;
+    sf::Sprite* defeat = new sf::Sprite();
+    sf::Sprite* defback = new sf::Sprite();
+    sf::RectangleShape hitboxHead, hitboxBody;
+    bool isHitboxDrawn = true;
+
+    Player* player;
+    double counter = 0;
+    float health;
+    int current = 0;
+    int windowWidth, windowHeight;
+    bool alive = true;
+    float posX, posY;
+public:
+
+    StaticOpponent(Player* player);
+
+    void setWindow(sf::RenderWindow* gameWindow);
+    void setSize(int width, int height);
+    void updateFrame(double time);
+    bool isAlive();
+    bool shoot(float X, float Y); //will call in arrow class
+};
