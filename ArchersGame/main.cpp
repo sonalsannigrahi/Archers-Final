1e0672444771f1a0a0bda57044b578f1a78f4b0f
saba-tavdgiridze <56112455+saba-tavdgiridze@users.noreply.github.com>
2020-01-11 03:23:31 -0500

    Update main.cpp

--- ArchersGame/main.cpp
+++ ArchersGame/main.cpp
@@ -1,8 +1,38 @@
 #include "Game/Game.hpp"
 
+#include "ARCADE_ARCHER/RETRO_GAME.hpp"
+#include "ARCADE_ARCHER/RETRO_GAME_SETTINGS.hpp"
+
+
+
 int main()
 {
     Game* game = new Game();
+    sf::RenderWindow* window = game->getWindow();
+
+    RetroGame retroGame = RetroGame(window);
+    
+    STATES::STATES_ENUM state = STATES::GAME_CLASSIC;
+
+    while(true)
+    {
+        if( state == STATES::DEAD )
+        {
+            window->close();
+            break;
+        }
+        else if(state == STATES::GAME_CLASSIC)
+        {
+            state = game->StartGame();
+        }
+        else if(state == STATES::GAME_RETRO)
+        {
+            state = retroGame.run();
+        }
+    }
+    
+
+    game->StartGame();
 
     return 0;
 }
